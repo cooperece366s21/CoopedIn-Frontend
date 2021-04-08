b3e93aa74fcd8fef6d9678ee4f14ec7fb639cb43
ArthurSkok <arthurskok00@gmail.com>
2021-04-07 20:09:23 -0400

    testing visuals

---
+++ client/src/component/Login/Login.tsx
@@ -0,0 +1,11 @@
+import {
+    Box,
+    Button,
+} from "@chakra-ui/react";
+const Test = () => {
+    return(
+        <Button
+
+            />
+    )
+}
--- client/src/index.js
+++ client/src/index.js
@@ -11,6 +11,21 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
+import React, { Component } from 'react'
+import { Button } from 'react-native'
+class App extends Component {
+
+    render() {
+        return (
+            <div>
+                <h1>Hello, Learner.Welcome to GeeksforGeeks.</h1>
+            </div>
+
+        );
+    }
+}
+export default Test
+// <Link to="/signup" className="btn btn-primary">Sign up</Link>
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
