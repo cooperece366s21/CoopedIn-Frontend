675dab7087d726bfb77254a7bdea5b25dd6bc0c6
ArthurSkok <arthurskok00@gmail.com>
2021-05-03 12:23:16 -0400

    tried adding auth, needs to be finished (don't have time for a day or two)

--- client/src/component/Login/Login.js
+++ client/src/component/Login/Login.js
@@ -1,7 +1,23 @@
 import React from "react";
 import "./Login.scss";
 import {withRouter} from "react-router-dom";
+import * as api from "../../services/api";
+
+
 export class Login extends React.Component{
+    state = {
+        username: "",
+        password: "",
+        loginResponse: null,
+
+    };
+    async onSubmit(){
+        const { username, password } = this.state;
+        const result = await api.login(username, password);
+        if (result.status ===! "success") {
+            this.setState({loginResponse: result.error});
+        }
+    }
     constructor(props) {
         super(props);
         this.handleClick = this.handleClick.bind(this);
@@ -11,24 +27,27 @@ export class Login extends React.Component{
         this.props.history.push("/userPage");
         window.location.reload()
     };
+
     render() {
         return(
             <div className="base-container">
                 <div className = "header">Login</div>
                 <div className = "content">
-                    <div className = "form">
+                    <form  onSubmit={e => e.preventDefault()}>
                         <div className ="form-group">
                             <label htmlFor="username">Username</label>
-                            <input type="text" name = "username" placeholder = "Username"/>
+                            <input type="text" name = "username" placeholder = "Username"
+                                   onChange={e => this.setState({ username: e.currentTarget.value })}/>
                         </div>
                         <div className ="form-group">
                             <label htmlFor="password">Password</label>
-                            <input type="password" name = "password" placeholder = "Password"/>
+                            <input type="password" name = "password" placeholder = "Password"
+                                   onChange={e => this.setState({ password: e.currentTarget.value })}/>
                         </div>
-                    </div>
+                    </form>
                 </div>
                 <div className = "footer">
-                    <button type ="button" onClick={this.handleClick} className ="btn">
+                    <button type ="button" onClick={() => this.onSubmit()} className ="btn">
                         Login
                     </button>
                 </div>
--- client/src/services/api.ts
+++ client/src/services/api.ts
@@ -1,13 +1,36 @@
 export const BACKEND_URL = "http://localhost:4567";
 
 function getCurrentUserId(): string {
-    return localStorage.getItem("user") || "";
+    return localStorage.getItem("authtoken") || "";
 }
 
-function setCurrentUser(user: User): void {
-    localStorage.setItem("user", user.id);
+function setCurrentUser(token: string): void {
+    localStorage.setItem("authtoken", token);
 }
 export type User = {
     id: string;
     name: string;
 };
+
+export async function login(
+    username: string,
+    password: string
+    )
+{
+    const response = await fetch(`${BACKEND_URL}/login`, {
+        method: "POST",
+        mode: "cors",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ username, password })
+    });
+
+    if (response.ok) {
+        let token: User = await response.json();
+        setCurrentUser(token);
+        return { value: token, status: "success" };
+    } else {
+        return { error: response.status.toString(), status: "failure" };
+    }
+}
